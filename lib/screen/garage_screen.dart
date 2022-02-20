573b3d823445caa305eead0d969828ef614bb4e1
Lisk0 <nemanjalisinac@gmail.com>
2022-02-20 10:24:31 +0100

    finished splash screen and transition to garage_screen

---
+++ lib/screen/garage_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GarageScreen extends StatefulWidget {
+  const GarageScreen({Key? key}) : super(key: key);
+
+  @override
+  _GarageScreenState createState() => _GarageScreenState();
+}
+
+class _GarageScreenState extends State<GarageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_fonts/google_fonts.dart';
+
+import './garage_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (_) => const GarageScreen()));
+    });
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Center(
+            child: Text(
+              'My garage',
+              style: GoogleFonts.merriweather(
+                fontSize: 64,
+                fontWeight: FontWeight.w800,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height / 2.5,
+              bottom: 8,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'Nemanja Lisinac',
+                  style: GoogleFonts.gideonRoman(
+                      fontSize: 32,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400),
+                ),
+                Text(
+                  '2021',
+                  style: GoogleFonts.gideonRoman(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
